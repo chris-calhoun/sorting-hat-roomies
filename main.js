0c61079602de0abb60fc703773273c37db7af084
Liz <liz@Lizs-Mac.local>
2020-08-06 21:28:13 -0500

    create bootstrap form in javascript using function that prints to the DOM

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 console.log('test');
+
+const renderToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+  };
+
+const createForm = () => {
+    const form =    `<h3>Enter First Year's Name</h3>
+                    <div class="formBody">
+                        <form class="form-inline" id="sortingForm">
+                            <div class="form-group mb-2">
+                                <label for="studentName" class="mr-2 text">Student : </label>
+                                <input type="text" class="form-control" id="inputName" placeholder="Hermione Granger">
+                            </div>
+                            <button type="submit" class="btn btn-primary mb-2" id="btnShowCards">Reveal my house!</button>
+                        </form>
+                    </div>`;
+
+renderToDom("containerForm", form);
+};
+
+createForm();
