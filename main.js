3fa71ebd4b7b99f5496d7e86588b1f92d3adfeb3
danapham <danapham12@gmail.com>
2020-08-06 22:22:03 -0500

    created an error message

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('test');
-
 const renderToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
@@ -17,12 +15,24 @@ const createForm = () => {
                             </div>
                             <button type="submit" class="btn btn-primary mb-2" id="btnShowCards">Reveal my house!</button>
                         </form>
-                    </div>`;
+                    </>`;
 
 renderToDom("containerForm", form);
 };
 
+const displayErrorMessage = () => {
+  const errorMessage = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                          <strong>Merlin's beard!</strong> How am I to sort ya without a name?!
+                          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>`;
+  renderToDom('containerErrorMessage', errorMessage);
+  }
+
 const domEvents = () => {
 document.querySelector('#btnShowForm').addEventListener('click', createForm);
+document.querySelector('#btnShowForm').addEventListener('click', displayErrorMessage);
 }
 
+domEvents();
