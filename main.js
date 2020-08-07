80598bf98b23ac11d6ee4c00e3af3d8d44fd7d2a
Liz <liz@Lizs-Mac.local>
2020-08-06 22:54:48 -0500

    add styling to jumbotron and form

--- main.js
+++ main.js
@@ -8,16 +8,18 @@ const renderToDom = (divId, textToPrint) => {
   };
 
 const createForm = () => {
-    const form =    `<h3>Enter First Year's Name</h3>
-                    <div class="formBody">
-                        <form class="form-inline" id="sortingForm">
-                            <div class="form-group mb-2">
-                                <label for="studentName" class="mr-2 text">Student : </label>
-                                <input type="text" class="form-control" id="inputName" placeholder="Hermione Granger">
-                            </div>
-                            <button type="submit" class="btn btn-primary mb-2" id="btnShowCards">Reveal my house!</button>
-                        </form>
-                    </div>`;
+    const form =    `<div class="wholeForm" style="border: 2px solid #345455;  width: 55%; margin: auto; padding: 25px 0px 20px 0px; border-radius: 10px; color: #345455;">
+                        <h3 class="text-center mb-3">Enter First Year's Name</h3>
+                        <div class="formBody d-flex justify-content-center">
+                            <form class="form-inline" id="sortingForm">
+                                <div class="form-group mb-2 p-2">
+                                    <label for="studentName" class="mr-2 text" style="color: #345455;">Student : </label>
+                                    <input type="text" class="form-control" id="inputName" placeholder="Hermione Granger">
+                                </div>
+                                <button type="submit" class="btn mb-2" id="btnShowCards" style="background-color: #F2C94C;">Reveal my house!</button>
+                            </form>
+                        </div>
+                    </div>;`
 
 renderToDom("containerForm", form);
 };
@@ -26,3 +28,4 @@ const domEvents = () => {
 document.querySelector('#btnShowForm').addEventListener('click', createForm);
 }
 
+domEvents();
